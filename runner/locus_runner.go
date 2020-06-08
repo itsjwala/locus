b4955613141b4c707ea041c3a77dea0077ca5962
Jigar Wala <jigar.w@browserstack.com>
2020-06-08 09:08:03 +0530

    deleted stale code

--- runner/locus_runner.go
+++ runner/locus_runner.go
@@ -4,21 +4,39 @@ import (
   "fmt"
   "os"
   "encoding/json"
+  "./languages"
 )
 
 type capability struct {
-  Code string  
-  Language string
+  code     string   `json:"code"`
+  language string   `json:"language"`
 }
 
+// type result struct {
+//   Stdout string     `json:"stdout"`
+//   Stderr string     `json:"stderr"`
+//   Error  string     `json:"error"`
+// }
 func main(){
 
-  json_blob := os.Args[1]
-  fmt.Println(json_blob)
-  var caps capability
 
-  if err := json.Unmarshal([]byte(json_blob),&caps) ; err != nil {
-    fmt.Println(err)
+  caps := &capability{}
+
+  if err := json.NewDecoder(os.Stdin).Decode(caps) ; err != nil {
+     fmt.Println(err)
+     panic(err)
   }
+
   fmt.Println(caps)
+  fmt.Println("Runnning")
+
+
+  if ! language.IsSupported(caps.language) {
+    panic("Language is not supported")
+  }
+
+
+  language.Run(caps.language,caps.code)
+   
+
 }
