043667ed7c919b6fc743f5124fd3a0ab1f11e553
Jigar Wala <jigar.w@browserstack.com>
2020-07-05 18:42:14 +0530

    feat: added nodejs runner

---
+++ runner/languages/nodejs/runner.go
@@ -0,0 +1,19 @@
+package nodejs
+
+import (
+	"os/exec"
+	"path/filepath"
+	"bytes"
+)
+
+func Run(codeFilePath string) (string, string , error){
+	cmd := exec.Command("node")
+	var stdout  bytes.Buffer	
+	var stderr  bytes.Buffer	
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	cmd.Dir = filepath.Dir(codeFilePath)	
+	cmd.Args = append(cmd.Args,filepath.Base(codeFilePath))
+	err := cmd.Run()
+	return stdout.String(),stderr.String(), err
+}
