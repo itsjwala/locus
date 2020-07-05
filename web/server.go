3f729c1c055ca1003fdd3e66f7382311ac014ecf
bhavnavarshney-continuum <bhavna.varshney@continuum.net>
2020-07-05 18:18:16 +0000

    Adding code to create, start and run container

--- web/server.go
+++ web/server.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
@@ -15,14 +18,26 @@ func main() {
 		panic(err)
 	}
 
-	
-
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	language := "python"
+	input := `{"language":"python", "code":"print(\"helloworld\")"}`
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: fmt.Sprintf("itsjwala/locus_runner-%s", language),
+		Cmd:   []string{input},
+	}, nil, nil, nil, "")
 	if err != nil {
 		panic(err)
 	}
 
-	for _, container := range containers {
-		fmt.Println(container)
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
 	}
+	fmt.Println(resp.ID)
+	options := types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true}
+	// Replace this ID with a container that really exists
+	out, err := cli.ContainerLogs(ctx, resp.ID, options)
+	if err != nil {
+		panic(err)
+	}
+
+	io.Copy(os.Stdout, out)
 }
